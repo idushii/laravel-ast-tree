04b90a96cec7c39d0a4848c4f5f1a65093d26766
Nikolay Ilinov <Nikolay.Ilinov@x5.ru>
2022-02-02 14:36:15 +0700

    use php paser result json

--- ast.php
+++ ast.php
@@ -11,15 +11,16 @@ function test($foo)
 }
 CODE;
 
-//$code2 = file_get_contents('./')
+$code2 = file_get_contents('app/Http/Resources/ObjectItemResource.php');
 
 $parser = (new \PhpParser\ParserFactory)->create(\PhpParser\ParserFactory::PREFER_PHP7);
 try {
-    $ast = $parser->parse($code);
+    $ast = $parser->parse($code2);
 } catch (Error $error) {
     echo "Parse error: {$error->getMessage()}\n";
     return;
 }
 
 $dumper = new \PhpParser\NodeDumper;
+file_put_contents('res.json', json_encode($ast));
 echo $dumper->dump($ast) . "\n";
