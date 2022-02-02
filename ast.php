a33fffc6068f49d7c34392e69361443ff1662f2b
Nikolay Ilinov <Nikolay.Ilinov@x5.ru>
2022-02-02 14:53:27 +0700

    test reflection api for ObjectItemResource

--- ast.php
+++ ast.php
@@ -2,24 +2,9 @@
 
 require 'vendor/autoload.php';
 
-$code = <<<'CODE'
-<?php
+$classReflection = new ReflectionClass('App\Http\Resources\ObjectItemResource');
+//
+//$object = new \App\Http\Resources\ObjectItemResource([]);
+//$objectReflection = new ReflectionObject($object);
 
-function test($foo)
-{
-    var_dump($foo);
-}
-CODE;
-
-//$code2 = file_get_contents('./')
-
-$parser = (new \PhpParser\ParserFactory)->create(\PhpParser\ParserFactory::PREFER_PHP7);
-try {
-    $ast = $parser->parse($code);
-} catch (Error $error) {
-    echo "Parse error: {$error->getMessage()}\n";
-    return;
-}
-
-$dumper = new \PhpParser\NodeDumper;
-echo $dumper->dump($ast) . "\n";
+var_dump($classReflection->getMethod('toArray')->getDocComment());
